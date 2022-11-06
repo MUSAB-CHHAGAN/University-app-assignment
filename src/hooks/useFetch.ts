0b51ce73e05fa9af14c51f93386aa426dc98386a
MUSAB-CHHAGAN <chhagan.musab98@gmail.com>
2022-11-06 20:44:22 +0530

    removed : headers

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -8,14 +8,7 @@ const useFetch = (url, mapCountriesDataAsRequired) => {
 
   const fetchCountries = async () => {
     setIsFetching(true);
-    const countriesData = await fetch(url, {
-      method: "GET",
-      mode: "no-cors",
-      headers: {
-        "access-control-allow-origin": "*",
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    });
+    const countriesData = await fetch(url);
     const parsedCountriesData = await countriesData.json();
     if (mapCountriesDataAsRequired) {
       const countriesWithRequiredFieldsOnly = parsedCountriesData.map(
